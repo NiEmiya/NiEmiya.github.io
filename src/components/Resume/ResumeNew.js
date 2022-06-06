64980b74a33865b452ad26cd59f716bcddcf864d
cybernaga <45783152+NiEmiya@users.noreply.github.com>
2022-06-06 12:55:26 -0400

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,8 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/niemiya/niemiya.github.io/master/src/Assets/Resume.pdf";
-
+  "https://raw.githubusercontent.com/NiEmiya/niemiya.github.io/main/src/Assets/Resume.pdf";
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 

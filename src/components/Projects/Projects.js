6f9d58ade4698cfd6fab07eafa46cdddbb588d88
cybernaga <45783152+NiEmiya@users.noreply.github.com>
2022-06-26 22:36:48 -0400

    Xd

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import game2048 from "../../Assets/Projects/2048.png";
+import crypto from "../../Assets/Projects/crypto.png";
+import chess from "../../Assets/Projects/chess.png";
+import greenman from "../../Assets/Projects/greenman.png";
+import sort from "../../Assets/Projects/sort.png";
 
 function Projects() {
   return (
@@ -25,60 +25,59 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Discord Clone"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging and voice chat."
-              link="https://github.com/niemiya/dotes-discord"
+              title="Chat Application"
+              description="Personal Chat Room with features such as realtime messaging, basic moderation and voice chat. Written in React.js, Material-UI, and Firebase."
+              link="https://niemiya.github.io/Chat-app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={crypto}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/soumyajit4419/Bits-0f-C0de"
+              title="Crypto App"
+              description="Sample Crypto information hub, built using numerous API's from Rapid API and React."
+              link="https://niemiya.github.io/Crypto-app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={chess}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              link="https://github.com/soumyajit4419/Editor.io"
+              title="Chess AI"
+              description="Chess AI written in Javascript using chess.js library"
+              link="https://niemiya.github.io/Chess-demo"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={sort}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              link="https://github.com/soumyajit4419/Plant_AI"
+              title="Sorting Demo"
+              description="A Visual sorting algorithm application with customizable speed, array length and different algorithms."
+              link="https://niemiya.github.io/Sorting-demo/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={game2048}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="2048"
+              description="Clone of the popular mobile game 2048 written in Javascript and React."
+              link="https://niemiya.github.io/2048-Clone"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={greenman}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="Greenman House Party NFT"
+              description="Website built in Next.Js for the Greenman NFT collection."
+              link="https://greenmanhouseparty.io/"
             />
           </Col>
         </Row>
